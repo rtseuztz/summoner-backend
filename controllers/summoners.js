a9f1e189be1922c860faf5417fdfadf6d39abf2e
rtseuztz <yoyoguy111@gmail.com>
2022-11-01 15:01:33 -0500

    fixed typo

--- controllers/summoners.js
+++ controllers/summoners.js
@@ -2,7 +2,7 @@ const { query } = require("../sql");
 const getUser = async (req, res) => {
   const name = req.params.name;
   console.log(query);
-  const rows = await query(`SELECT * FROM summoners WHERE name = '${name}'`);
+  const rows = await query(`SELECT * FROM Summoners WHERE name = '${name}'`);
   res.send(rows);
 };
 exports.getUser = getUser;
