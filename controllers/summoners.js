c7a63ef37a07f2edfe1c409f5716edd0bc45b1bc
rtseuztz <yoyoguy111@gmail.com>
2022-11-01 18:23:07 -0500

    migrated to axios

--- controllers/summoners.js
+++ controllers/summoners.js
@@ -1,26 +1,27 @@
 const { query } = require("../sql");
-const fetch = require("node-fetch");
+const axios = require("axios");
 const getUser = async (req, res) => {
   //get the user from the riot api
   const name = req.params.name;
   const api_key = process.env.RIOT_API_KEY;
   const url = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${api_key}`;
-  const response = await fetch(url);
-  const json = await response.json();
+  const { data } = await axios.get(url);
+  //const response = await fetch(url);
+  //const json = await response.json();
   const summoner = new Summoner({
-    name: json.name,
-    accountId: json.accountId,
-    id: json.id,
-    puuid: json.puuid,
-    profileIconId: json.profileIconId,
-    revisionDate: json.revisionDate,
-    summonerLevel: json.summonerLevel,
+    name: data.name,
+    accountId: data.accountId,
+    id: data.id,
+    puuid: data.puuid,
+    profileIconId: data.profileIconId,
+    revisionDate: data.revisionDate,
+    summonerLevel: data.summonerLevel,
   });
   //upload the user to the database
   console.log(summoner);
   const queryStr = `INSERT INTO summoners (name, accountId, id, puuid, profileIconId, revisionDate, summonerLevel) VALUES ('${summoner.name}', '${summoner.accountId}', '${summoner.id}', '${summoner.puuid}', '${summoner.profileIconId}', '${summoner.revisionDate}', '${summoner.summonerLevel}')`;
   const rows = await query(queryStr);
-  res.send(rows);
+  res.send(data);
   // const name = req.params.name;
   // console.log(query);
   // const rows = await query(`SELECT * FROM Summoners WHERE name = '${name}'`);
