34ef2b04146e2c93bbe6374ab55da24126fc5c86
rtseuztz <yoyoguy111@gmail.com>
2022-11-01 18:08:30 -0500

    changed functionality

--- controllers/summoners.js
+++ controllers/summoners.js
@@ -1,8 +1,49 @@
 const { query } = require("../sql");
+const fetch = require("node-fetch");
 const getUser = async (req, res) => {
+  //get the user from the riot api
   const name = req.params.name;
-  console.log(query);
-  const rows = await query(`SELECT * FROM Summoners WHERE name = '${name}'`);
+  const api_key = process.env.RIOT_API_KEY;
+  const url = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${api_key}`;
+  const response = await fetch(url);
+  const json = await response.json();
+  const summoner = new Summoner({
+    name: json.name,
+    accountId: json.accountId,
+    id: json.id,
+    puuid: json.puuid,
+    profileIconId: json.profileIconId,
+    revisionDate: json.revisionDate,
+    summonerLevel: json.summonerLevel,
+  });
+  //upload the user to the database
+  console.log(summoner);
+  const queryStr = `INSERT INTO summoners (name, accountId, id, puuid, profileIconId, revisionDate, summonerLevel) VALUES ('${summoner.name}', '${summoner.accountId}', '${summoner.id}', '${summoner.puuid}', '${summoner.profileIconId}', '${summoner.revisionDate}', '${summoner.summonerLevel}')`;
+  const rows = await query(queryStr);
   res.send(rows);
+  // const name = req.params.name;
+  // console.log(query);
+  // const rows = await query(`SELECT * FROM Summoners WHERE name = '${name}'`);
+  // res.send(rows);
 };
 exports.getUser = getUser;
+
+class Summoner {
+  constructor({
+    name,
+    accountId,
+    id,
+    puuid,
+    profileIconId,
+    revisionDate,
+    summonerLevel,
+  }) {
+    this.name = name;
+    this.accountId = accountId;
+    this.id = id;
+    this.puuid = puuid;
+    this.profileIconId = profileIconId;
+    this.revisionDate = revisionDate;
+    this.summonerLevel = summonerLevel;
+  }
+}
--- server.js
+++ server.js
@@ -6,16 +6,6 @@ const { query } = require("./sql.js");
 const summoners_routes = require("./routes/summoner.js");
 const PORT = 8080;
 const HOST = "0.0.0.0";
-// async function getTodos() {
-//   let conn;
-//   try {
-//     conn = await pool.getConnection();
-//     const rows = await conn.query("SELECT * FROM todos");
-//     return rows;
-//   } catch (err) {
-//     return err;
-//   }
-// }
 
 // App
 const app = express();
@@ -28,12 +18,9 @@ app.get("/todos", async (req, res) => {
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
 app.use("/summoners", summoners_routes);
+
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
 });
-// router.get("/summoners/:name", async (req, res) => {
-//   const name = req.params.name;
-//   const rows = await query(`SELECT * FROM summoners WHERE name = '${name}'`);
-//   res.send(rows);
-// });
